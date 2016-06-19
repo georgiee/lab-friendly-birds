a1a5dd7338d8a9adb12ed7beb6227bc79060c0fa
Georgios Kaleadis <georgios@kaleadis.de>
2016-06-19 15:31:50 +0200

    add chromeless

--- js/friendlybirds/debug/stats-instance.js
+++ js/friendlybirds/debug/stats-instance.js
@@ -4,7 +4,7 @@ define(['stats', 'environment'], function(Stats, environment){
   s.setMode(0);
   
   s.domElement.style.position = 'absolute';
-  s.domElement.style.right = '0px';
+  s.domElement.style.left = '0px';
   s.domElement.style.bottom = '0px';
 
   if(environment.development){
